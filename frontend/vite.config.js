38cadd66cac7c2e0d78849e343489afa5cbe6922
smy619 <solene.s2025@gmail.com>
2025-11-07 22:40:15 +0100

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-   optimizeDeps: {
-    include: ['framer-motion'],
+  base: "/", 
+
+
+  optimizeDeps: {
+    include: ["framer-motion", "aos", "glightbox", "swiper"],
   },
+
+
   resolve: {
-    dedupe: ['react', 'react-dom'], 
+    dedupe: ["react", "react-dom"],
   },
-})
+
+
+  build: {
+    sourcemap: false, 
+    minify: "esbuild", 
+    cssMinify: true,
+    chunkSizeWarningLimit: 500, 
+
+    rollupOptions: {
+      output: {
+       
+        manualChunks: {
+          react: ["react", "react-dom"],
+          vendor: ["aos", "glightbox", "swiper", "framer-motion"],
+        },
+      },
+    },
+  },
+});
